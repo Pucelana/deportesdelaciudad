4b0349be9a3ffe58bb4c9baac3cf87c1f25d7d6c
Pucela83 <vane_euro@hotmail.com>
2025-04-07 23:25:04 +0200

    uemc

---
+++ app/models/calend.py
@@ -0,0 +1,18 @@
+from app.extensions import db
+
+class JornadaUEMC(db.Model):
+    __tablename__ = 'jornadas_uemc'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    partidos = db.relationship('UEMCPartido', backref='jornada', cascade='all, delete-orphan')
+
+class UEMCPartido(db.Model):
+    __tablename__ = 'uemc_partidos'
+    id = db.Column(db.Integer, primary_key=True)
+    jornada_id = db.Column(db.Integer, db.ForeignKey('jornadas_uemc.id'), nullable=False)
+    fecha = db.Column(db.Date, nullable=False)
+    hora = db.Column(db.Time, nullable=False)
+    local = db.Column(db.String(255))
+    resultadoA = db.Column(db.String(120))
+    resultadoB = db.Column(db.String(120))
+    visitante = db.Column(db.String(255))   
--- run.py
+++ run.py
@@ -1,19 +1,13 @@
 from app import create_app
 from app.extensions import db
 from app.models.horario import Horario
-
-
+from app.models.calend import JornadaUEMC, UEMCPartido
 
 app = create_app()
 
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['CACHE_TYPE'] = 'null'  # Esto desactiva el caché
 app.debug = True 
-
-with app.app_context():
-    db.create_all()
-
-
 
 
 if __name__=="__main__":
