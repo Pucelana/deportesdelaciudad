441e43ee16eeedd598cb52684864af5785553063
Pucelana <https://github.com/Pucelana/school_adventure.git>
2026-06-04 22:48:08 +0200

    track

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -31,7 +31,20 @@ document.addEventListener("DOMContentLoaded", () => {
     /* =========================
        3. AUTO SCROLL (NETFLIX STYLE)
     ========================== */
-    let speed = 0.8;
+    let speed;
+    function setSpeed() {
+    const width = window.innerWidth;
+
+    if (width <= 576) {
+        speed = 0.25; // móvil (más suave)
+    } else if (width <= 992) {
+        speed = 0.4; // tablet
+    } else {
+        speed = 0.6; // desktop
+    }
+}
+setSpeed();
+window.addEventListener('resize', setSpeed);
     let isPaused = false;
 
     function autoScroll() {
@@ -78,6 +91,61 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const x = e.pageX - track.offsetLeft;
         const walk = (x - startX) * 2;
+
+        track.scrollLeft = scrollLeft - walk;
+    });
+
+});
+
+/* Admin */
+document.addEventListener("DOMContentLoaded", () => {
+
+    const track = document.querySelector('.admin-track');
+    if (!track) return;
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    track.addEventListener('mousedown', (e) => {
+        isDown = true;
+        track.classList.add('active');
+
+        startX = e.pageX - track.offsetLeft;
+        scrollLeft = track.scrollLeft;
+    });
+
+    track.addEventListener('mouseleave', () => {
+        isDown = false;
+        track.classList.remove('active');
+    });
+
+    track.addEventListener('mouseup', () => {
+        isDown = false;
+        track.classList.remove('active');
+    });
+
+    track.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+
+        const x = e.pageX - track.offsetLeft;
+        const walk = (x - startX) * 2;
+
+        track.scrollLeft = scrollLeft - walk;
+    });
+
+    // soporte táctil
+    let touchStartX = 0;
+
+    track.addEventListener('touchstart', (e) => {
+        touchStartX = e.touches[0].pageX;
+        scrollLeft = track.scrollLeft;
+    });
+
+    track.addEventListener('touchmove', (e) => {
+        const x = e.touches[0].pageX;
+        const walk = (x - touchStartX) * 2;
 
         track.scrollLeft = scrollLeft - walk;
     });
