826c3abcda73bed88e556cbfec68bab3a9b792f8
Pucelana <https://github.com/Pucelana/school_adventure.git>
2026-07-01 19:17:09 +0200

    fecha

--- app/routes/aula_route.py
+++ app/routes/aula_route.py
@@ -86,6 +86,7 @@ def modificar_jornada_aula(id):
                 # Obtener el partido correspondiente por ID
                 partido = db.session.query(AulaPartido).filter(AulaPartido.id == partido_id).first()
                 if partido:
+                    partido.fecha = fecha
                     partido.hora = hora
                     partido.local = local
                     partido.resultadoA = resultadoA
