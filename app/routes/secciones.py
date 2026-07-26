eecd572cf268d595132bb614d6bcf9c75de20380
Pucelana <https://github.com/Pucelana/school_adventure.git>
2026-07-26 21:59:01 +0200

    comercial

--- app/routes/secciones.py
+++ app/routes/secciones.py
@@ -1,83 +1,93 @@
 from flask import Blueprint, render_template
+from app.models.comercial import COMERCIAL
 
 secciones_bp = Blueprint('secciones', __name__)
+
+def render_seccion(template, nombre, zona='seccion'):
+    comercial = COMERCIAL.get(nombre, {})
+
+    return render_template(
+        template,
+        comercial=comercial,
+        zona=zona
+    )
 
 # Rutas de los equipos
 @secciones_bp.route('/seccion/uemc')
 def seccion_uemc():
-    return render_template('secciones/uemc.html')
+    return render_seccion('secciones/uemc.html', 'uemc')
 
 @secciones_bp.route('/seccion/ponce')
 def seccion_ponce():
-    return render_template('secciones/ponce.html')
+    return render_seccion('secciones/ponce.html', 'ponce')
 
 @secciones_bp.route('/seccion/cdsi_vall')
 def seccion_cdsi_vall():
-    return render_template('secciones/cdsi_vall.html')
+    return render_seccion('secciones/cdsi_vall.html', 'vall_sala')
 
 @secciones_bp.route('/seccion/vcv')
 def seccion_vcv():
-    return render_template('secciones/vcv.html')
+    return render_seccion('secciones/vcv.html', 'vcv')
 
 @secciones_bp.route('/seccion/san_jose')
 def seccion_san_jose():
-    return render_template('secciones/san_jose.html')
+    return render_seccion('secciones/san_jose.html', 'san_jose')
 
 @secciones_bp.route('/seccion/aliados')
 def seccion_aliados():
-    return render_template('secciones/aliados.html')
+    return render_seccion('secciones/aliados.html', 'aliados')
 
 @secciones_bp.route('/seccion/aula')
 def seccion_aula():
-    return render_template('secciones/aula.html')
+    return render_seccion('secciones/aula.html', 'aula')
 
 @secciones_bp.route('/seccion/recoletas')
 def seccion_recoletas():
-    return render_template('secciones/recoletas.html')
+    return render_seccion('secciones/recoletas.html', 'recoletas')
 
 @secciones_bp.route('/seccion/valladolid')
 def seccion_valladolid():
-    return render_template('secciones/valladolid.html')
+    return render_seccion('secciones/valladolid.html', 'valladolid')
 
 @secciones_bp.route('/seccion/promesas')
 def seccion_promesas():
-    return render_template('secciones/promesas.html')
+    return render_seccion('secciones/promesas.html', 'promesas')
 
 @secciones_bp.route('/seccion/caja')
 def seccion_caja():
-    return render_template('secciones/caja.html')
+    return render_seccion('secciones/caja.html', 'caja')
 
 @secciones_bp.route('/seccion/panteras')
 def seccion_panteras():
-    return render_template('secciones/panteras.html')
+    return render_seccion('secciones/panteras.html', 'panteras')
 
 @secciones_bp.route('/seccion/vrac')
 def seccion_vrac():
-    return render_template('secciones/vrac.html')
+    return render_seccion('secciones/vrac.html', 'vrac')
 
 @secciones_bp.route('/seccion/salvador')
 def seccion_salvador():
-    return render_template('secciones/salvador.html')
+    return render_seccion('secciones/salvador.html', 'salvador')
 
 @secciones_bp.route('/seccion/salvador_fem')
 def seccion_salvador_fem():
-    return render_template('secciones/salvador_fem.html')
+    return render_seccion('secciones/salvador_fem.html', 'salvador_fem')
 
 @secciones_bp.route('/seccion/rv_femenino')
 def seccion_rv_femenino():
-    return render_template('secciones/rv_femenino.html')
+    return render_seccion('secciones/rv_femenino.html', 'rv_femenino')
 
 @secciones_bp.route('/seccion/parquesol')
 def seccion_parquesol():
-    return render_template('secciones/parquesol.html')
+    return render_seccion('secciones/parquesol.html', 'parquesol')
 
 @secciones_bp.route('/seccion/galvan')
 def seccion_galvan():
-    return render_template('secciones/galvan.html')
+    return render_seccion('secciones/galvan.html', 'galvan')
 
 @secciones_bp.route('/seccion/vall_sala')
 def seccion_vall_sala():
-    return render_template('secciones/vall_sala.html')
+    return render_template('secciones/vall_sala.html', 'vall_sala')
 
 # Rutas de sistemas de ligas
 @secciones_bp.route('/sistema_ligas/futbol')
