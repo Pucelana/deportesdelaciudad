252cc6e39bd0fa90343fd9ba735d8d49bd1b33ea
Pucelana <https://github.com/Pucelana/school_adventure.git>
2026-07-30 21:35:13 +0200

    seo1

--- app/seo/sitemap.py
+++ app/seo/sitemap.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response
+from flask import Blueprint, Response, current_app
 from datetime import datetime
 
 seo_bp = Blueprint("seo", __name__)
@@ -8,8 +8,26 @@ seo_bp = Blueprint("seo", __name__)
 def sitemap():
     hoy = datetime.utcnow().strftime("%Y-%m-%d")
 
+    urls = []
+
+    for rule in current_app.url_map.iter_rules():
+
+        # Excluir rutas del panel de administración
+        if rule.rule.startswith("/admin"):
+            continue
+
+        # Excluir archivos estáticos
+        if rule.rule.startswith("/static"):
+            continue
+
+        # Excluir rutas con parámetros
+        if len(rule.arguments) > 0:
+            continue
+
+    urls.append(rule.rule)
+
     xml = f"""<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
     <url>
         <loc>https://deportesdelaciudad.es/</loc>
@@ -18,7 +36,8 @@ def sitemap():
         <priority>1.0</priority>
     </url>
 
-</urlset>
-"""
+    </urlset>
+    """
+    return "<br>".join(sorted(urls))
 
-    return Response(xml, mimetype="application/xml")
+    """return Response(xml, mimetype="application/xml")"""
